139be765c224c970e1596161d5f9ed724814e37f
Dawood008 <kamranchem08@gmail.com>
2024-11-29 20:26:07 +0800

    initial commit of my website

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Example of a simple JavaScript function to display a message
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website is loaded!");
+});
+<script src="js/script.js"></script>
